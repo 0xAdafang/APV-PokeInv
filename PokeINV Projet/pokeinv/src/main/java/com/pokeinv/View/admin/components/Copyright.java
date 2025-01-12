e8365358f339fae07ee643321e3b882081c1ce07
Djamel Nait Sidenas <xamel.ns@gmail.com>
2025-01-12 04:55:11 -0500

    add copyright panel

---
+++ PokeINV Projet/pokeinv/src/main/java/com/pokeinv/View/admin/components/Copyright.java
@@ -0,0 +1,25 @@
+package com.pokeinv.View.admin.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Copyright extends JPanel {
+    public Copyright() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBackground(new Color(28, 28, 51));
+
+        JLabel copyright = new JLabel(
+                """
+                    <html>© 2025 PokeInv. <br>Tous droits réservés.<br>
+                    Fait par: <br>Nait Sidena Djamel<br>
+                    Sionneau Terence<br>Espinoza Kevin </html>
+                """,
+                SwingConstants.CENTER);
+        copyright.setForeground(Color.WHITE);
+        copyright.setFont(new Font("Arial", Font.BOLD, 12));
+
+        add(copyright);
+        add(Box.createRigidArea(new Dimension(0, 5)));
+
+    }
+}
